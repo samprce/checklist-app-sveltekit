2b8a6cef6b016856946126b06752bf082fe01db8
samprce <samprce@uab.edu>
2023-04-23 10:33:05 -0500

    added page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
-<h1 class="m-4 p-4 text-3xl font-bold text-red-400">Hello world!</h1>
-
-<style lang="postcss">
-	:global(html) {
-		background-color: theme(colors.gray.100);
-	}
-</style>
+<div class="min-h-screen bg-olive-one p-0 selection:bg-green-two md:px-8 md:py-24">
+	<main
+		class="md:outline-creme-four mx-auto min-h-screen max-w-none rounded-none bg-cream-four px-5 py-10 outline-none md:min-h-max md:max-w-[60rem] md:rounded-2xl md:px-8 md:outline md:outline-4 md:outline-offset-8"
+	>
+		<h1 class="text-2xl font-bold text-gray-three">Dashboard</h1>
+		<div class="mt-10" />
+	</main>
+</div>
