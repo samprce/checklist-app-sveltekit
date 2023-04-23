5f69d95351766682b64478259ba51255b44df274
samprce <samprce@uab.edu>
2023-04-23 12:30:58 -0500

    added todoinput and todosection

--- src/lib/components/todo/AddTodoSection.svelte
+++ src/lib/components/todo/AddTodoSection.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <section class="flex justify-between">
-	<h3 class="text-xl font-bold text-gray-three">Checklist</h3>
+	<h3 class="text-2xl font-bold text-gray-three">Checklist</h3>
 	<button
 		class="group flex items-center rounded-md border-cream-four bg-green-one px-6 py-3 text-lg font-semibold text-gray-five outline outline-2 outline-offset-2 outline-green-one hover:text-green-five focus-visible:text-green-five focus-visible:outline-green-five"
 		type="button"
